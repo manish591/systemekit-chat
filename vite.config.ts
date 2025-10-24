c93978ac7cc11751898417888aadf89689ba62d2
Manish Devrani <manishdevrani777@gmail.com>
2025-10-25 02:22:12 +0530

    fixed path not resolving issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import { tanstackRouter } from '@tanstack/router-plugin/vite'
+import path from "node:path";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,4 +12,9 @@ export default defineConfig({
     }),
     react()
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
